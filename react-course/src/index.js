d1a8c49f968fae69765d760fe427d9fe3e21ff6f
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-26 14:18:23 +0100

    Functional components

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -11,7 +11,22 @@ function MyApp(){
 )
 }
 
+function MyInfo(){
+  return(
+    <div>
+      <h1>My Name</h1>
+      <p>A little blurb about myself</p>
+      <h3>My top 3 vacation spots</h3>
+      <ul> 
+        <li>1st spot</li>
+        <li>2nd spot</li>
+        <li>3rd spot</li>
+      </ul>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <MyApp />,
+  <MyInfo />,
   document.getElementById('root')
 );
