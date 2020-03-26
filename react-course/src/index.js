29689332b2bc3e9bae5cb51553478dd815fdcdda
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-26 14:34:36 +0100

    Components should be in their own files

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,30 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyApp(){
-  return (
-  <ul>
-    <li>first</li>
-    <li>second</li>
-    <li>third</li>
-  </ul>
-)
-}
-
-function MyInfo(){
-  return(
-    <div>
-      <h1>My Name</h1>
-      <p>A little blurb about myself</p>
-      <h3>My top 3 vacation spots</h3>
-      <ul> 
-        <li>1st spot</li>
-        <li>2nd spot</li>
-        <li>3rd spot</li>
-      </ul>
-    </div>
-  )
-}
+import MyInfo from "./components/MyInfo.js"
 
 ReactDOM.render(
   <MyInfo />,
