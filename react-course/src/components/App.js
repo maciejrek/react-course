d5ab4b816f87d7536af9a8d7c5bede00ead49c21
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-27 15:24:58 +0100

    TODO app p3

--- react-course/src/components/App.js
+++ react-course/src/components/App.js
@@ -1,23 +1,22 @@
-import React from "react";
+import React,{Component} from "react";
 import "../style.css";
 import TodoItem from "./TodoItem";
 import todoData from './todoData'
 
 
-const App = () => {
-  const todoComponents = todoData.map(todo => (
-   <TodoItem key={todo.id} text={todo.text} completed={todo.completed}/>
-  ))
-
-  return <div className="todo-list">{todoComponents}</div>
-
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      todo:todoData
+    }
+  }
+  render()
+  {
+    const todoComponents = this.state.todo.map(todo => (
+      <TodoItem key={todo.id} text={todo.text} completed={todo.completed}/>
+     ))
+     return <div className="todo-list">{todoComponents}</div>
+  }
 }
-//   <div>
-//     <div className="todo-list">
-//       <TodoItem />
-//       <TodoItem />
-//       <TodoItem />
-//     </div>
-//   </div>
-// );
 export default App;
