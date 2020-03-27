56d22282ec8691de5743eaad145df0dbc555ced9
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-27 13:26:12 +0100

    Class components

--- react-course/src/component_workshop/AppWorkshop.js
+++ react-course/src/component_workshop/AppWorkshop.js
@@ -2,17 +2,34 @@ import React from "react";
 import ContactCard from "./ContactCard";
 import Joke from "./Joke";
 import jokesData from "./jokesData";
-import Product from './Product'
-import products from './vschoolProducts'
+import Product from "./Product";
+import products from "./vschoolProducts";
 
-function AppWorkshop() {
-//   const jokeComponents = jokesData.map(joke => (
-//     <Joke key={joke.id} question={joke.question} response={joke.response} />
-//   ));
-  const productComponents = products.map(product => (
-    <Product key={product.id} name={product.name}  price={product.price} description={product.description}/>
-  ))
+// function AppWorkshop() {
+// //   const jokeComponents = jokesData.map(joke => (
+// //     <Joke key={joke.id} question={joke.question} response={joke.response} />
+// //   ));
+//   const productComponents = products.map(product => (
+//     <Product key={product.id} name={product.name}  price={product.price} description={product.description}/>
+//   ))
 
-  return <div className="products">{productComponents}</div>;
+//   return <div className="products">{productComponents}</div>;
+// }
+
+class AppWorkshop extends React.Component {
+  ownMethodHer() {}
+  render() {
+    const productComponents = products.map(product => (
+      <Product
+        key={product.id}
+        name={product.name}
+        price={product.price}
+        description={product.description}
+      />
+    ));
+
+    return <div className="products">{productComponents}</div>;
+  }
 }
+
 export default AppWorkshop;
